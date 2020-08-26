54e9f5daf57ba2ba62740f7faf589ab935c8dc1b
demitelix <demiteli@inbox.lv>
2020-08-26 21:36:16 +0300

    pass in file

--- GenealogicalTrees/src/main/java/app/controller/FindPage.java
+++ GenealogicalTrees/src/main/java/app/controller/FindPage.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import app.database.AppConnect;
+
 
 @Controller
 public class FindPage {
 
 	@RequestMapping(value = {"/finds"}, method = RequestMethod.GET)
 	public String finds(Model model) {
-
+		
+		AppConnect.getAppProp();
 		return "find"; //<- it is name of HTML file without .html
 	}
 
